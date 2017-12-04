4cafd2c5b822762686d35dedcde6748752decc0a
SharonYi <yisl14@163.com>
2017-12-04 21:46:26 +0800

    update

---
+++ client/pages/my/index-2.js
@@ -0,0 +1,53 @@
+var app = getApp()
+Page( {
+  data: {
+    userInfo: {},
+    projectSource: 'https://github.com/liuxuanqiang/wechat-weapp-mall',
+    userListInfo: [ {
+      id: 'myorder',
+      icon: '../../images/iconfont-dingdan.png',
+      text: '我的订单',
+      isunread: true,
+      unreadNum: 2,
+      pages: ['../order']
+    }, {
+        icon: '../../images/iconfont-card.png',
+        text: '我的代金券',
+        isunread: false,
+        unreadNum: 2,
+        pages: ['pages/coupon']
+      }, {
+        icon: '../../images/info.png',
+        text: '绑定info账号',
+        isunread: true,
+        unreadNum: 1,
+        pages: ['pages/account']
+      }, {
+        icon: '../../images/barcode_2d.png',
+        text: '我的二维码'
+      }, {
+        icon: '../../images/iconfont-kefu.png',
+        text: '联系客服',
+        
+     }, {
+        icon: '../../images/iconfont-help.png',
+        text: '常见问题',
+        pages: ['pages/questions']
+      }], 
+
+  },
+
+  onLoad: function() {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo( function( userInfo ) {
+      //更新数据
+      that.setData( {
+        userInfo: userInfo
+      })
+    })
+  },
+
+  
+  
+})
--- client/pages/my/index.js
+++ client/pages/my/index.js
@@ -1,53 +1,142 @@
-var app = getApp()
-Page( {
+const app = getApp()
+
+Page({
   data: {
-    userInfo: {},
-    projectSource: 'https://github.com/liuxuanqiang/wechat-weapp-mall',
-    userListInfo: [ {
-      id: 'myorder',
-      icon: '../../images/iconfont-dingdan.png',
-      text: '我的订单',
-      isunread: true,
-      unreadNum: 2,
-      pages: ['pages/order']
-    }, {
-        icon: '../../images/iconfont-card.png',
-        text: '我的代金券',
-        isunread: false,
-        unreadNum: 2,
-        pages: ['pages/coupon']
-      }, {
-        icon: '../../images/info.png',
-        text: '绑定info账号',
-        isunread: true,
-        unreadNum: 1,
-        pages: ['pages/account']
-      }, {
-        icon: '../../images/barcode_2d.png',
-        text: '我的二维码'
-      }, {
-        icon: '../../images/iconfont-kefu.png',
-        text: '联系客服',
-        
-     }, {
-        icon: '../../images/iconfont-help.png',
-        text: '常见问题',
-        pages: ['pages/questions']
-      }], 
+    balance: 0,
+    freeze: 0,
+    score: 0,
+    score_sign_continuous: 0
+  },
+  onLoad() {
 
   },
-
-  onLoad: function() {
+  onShow() {
+    this.getUserInfo();
+    this.setData({
+      version: app.globalData.version
+    });
+    this.getUserApiInfo();
+    this.getUserAmount();
+    this.checkScoreSign();
+  },
+  getUserInfo: function (cb) {
     var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo( function( userInfo ) {
-      //更新数据
-      that.setData( {
-        userInfo: userInfo
-      })
+    wx.login({
+      success: function () {
+        wx.getUserInfo({
+          success: function (res) {
+            that.setData({
+              userInfo: res.userInfo
+            });
+          }
+        })
+      }
+    })
+  },
+  aboutUs: function () {
+    wx.showModal({
+      title: '关于我们',
+      content: '祝大家使用愉快！',
+      showCancel: false
     })
   },
 
+  contactUs: function () {
+    wx.showModal({
+      title: '联系客服',
+      content: '客服电话：010-627*****',
+      showCancel: false
+    })
+  },
   
-  
+  getUserApiInfo: function () {
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/user/detail',
+      data: {
+        token: app.globalData.token
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.setData({
+            apiUserInfoMap: res.data.data,
+            userMobile: res.data.data.base.mobile
+          });
+        }
+      }
+    })
+
+  },
+  getUserAmount: function () {
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/user/amount',
+      data: {
+        token: app.globalData.token
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.setData({
+            balance: res.data.data.balance,
+            freeze: res.data.data.freeze,
+            score: res.data.data.score
+          });
+        }
+      }
+    })
+
+  },
+  checkScoreSign: function () {
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/score/today-signed',
+      data: {
+        token: app.globalData.token
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.setData({
+            score_sign_continuous: res.data.data.continuous
+          });
+        }
+      }
+    })
+  },
+  scoresign: function () {
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/score/sign',
+      data: {
+        token: app.globalData.token
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.getUserAmount();
+          that.checkScoreSign();
+        } else {
+          wx.showModal({
+            title: '错误',
+            content: res.data.msg,
+            showCancel: false
+          })
+        }
+      }
+    })
+  },
+  relogin: function () {
+    var that = this;
+    app.globalData.token = null;
+    app.login();
+    wx.showModal({
+      title: '提示',
+      content: '重新登陆成功',
+      showCancel: false,
+      success: function (res) {
+        if (res.confirm) {
+          that.onShow();
+        }
+      }
+    })
+  },
+ 
 })
